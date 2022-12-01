f15b2b7201efa36a1e692fbde739914be47c6501
dustin4d <dustinholder14@yahoo.com>
2022-12-01 11:24:51 -0600

    addEventListener on each tile. Requires Gameboard.mapArray to be called

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ const Controller = (() => {
     // Invoke the function with arrow syntax
     coinflipBtn.addEventListener('click', () => {assignTeam()})
 
+    const showBoard = () => {
+        /* Board is hidden by default, and this function should
+         * change the classlist on that element to show it */
+    }
+
     const assignTeam = () => {
         // Random number between 0 and 1
         const coin = Math.floor(Math.random() * 2)
@@ -28,25 +33,27 @@ const Controller = (() => {
         coinflipBtn.remove()
     }
 
-    const showTeams = () => {
-        const coinflip = document.querySelector('.coinflip')
-        const teamDisplay = document.createElement('p')
-        teamDisplay.innerText(`You are ${Player.team}`)
-        coinflip.append(teamDisplay)
-    }
-
     // Controller Exports
     return {assignTeam}
 })()
 
 // Gameboard Object
 const Gameboard = (() => {
+    const tiles = document.querySelectorAll('.tile')
+
+    const mapArray = () => {
+        for (let i = 0; i < tiles.length; i++) {
+            tiles[i].addEventListener('click', () => {console.log("WOO")})
+        }
+    }
+    
+
     const board = ['X', 'X', 'X',
                    'O', 'O', 'O',
                    'X', 'X', 'X'
                 ]
     // Gameboard exports
-    return {board}
+    return {board, tiles, mapArray}
 })()
 
 // Player Object
