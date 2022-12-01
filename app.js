d2ae83d1cfaecdac4b11074fcc840487fb207962
dustin4d <dustinholder14@yahoo.com>
2022-12-01 10:24:31 -0600

    Team assignment

--- app.js
+++ app.js
@@ -1,73 +1,52 @@
-// Gameboard Object
-const Gameboard = (() => {
-    let score
-    
-    // Tile eventListener
-    const tile = document.querySelectorAll('.tile')
-    console.log(tile)
 
-   const board = []
-    
-    const buildBoard = () => {
-        /* Create all 9 DOM nodes as divs
-         * and append them to .board
-         */
-        for (i = 0; i < 9; i++) {
-            
+const Controller = (() => {
+    const assignTeam = () => {
+        // Random number between 0 and 1
+        const coin = Math.floor(Math.random() * 2)
+        console.log(`Flipping coin...`)
+        console.log(`Coin value is ${coin}`)
+
+        if (coin === 0) {
+            Player.team = 'X'
+            Cpu.team = 'O'
+        } else if (coin === 1) {
+            Player.team = 'O'
+            Cpu.team = 'X'
+        } else {
+            console.log("Error")
+            alert("Error")
         }
     }
-
-    const mark = (team) => {
-        /* Grab the player's or CPU's team
-        * indentifier (X or O), then write that value
-        * to the array index that corresponds to the DOM
-        * node that was clicked.
-        */
-    }
-
-    const arrayLink = () => {
-        /* Associate each array index to the DOM
-         * 0  1  2
-         * 3  4  5
-         * 6  7  8
-        */
-    }
-
-    // Gameboard exports
-    return {buildBoard, board, tile}
+    // Controller Exports
+    return {assignTeam}
 })()
 
+// Gameboard Object
+const Gameboard = (() => {
+    const board = ['X', 'X', 'X',
+                   'O', 'O', 'O',
+                   'X', 'X', 'X'
+                ]
+    // Gameboard exports
+    return {board}
+})()
 
 // Player Object
 const Player = (() => {
     let team
 
-    const pickTeam = () => {
-        /* Get value of a DOM node,
-         * do some light conditional formatting,
-         * then set player.team to be whatever value
-         * is returned from this function */
-    }
-
     // Player exports
     return {team}
 })()
 
-
 // CPU Object
 const Cpu = (() => {
-    //let team = oppositeTeam()
-
-    // Picks the symbol of whatever is not picked by Player
-    const oppositeTeam = () => {
-        if (Player.team === 'X') {
-            team = 'O'
-        } else {
-            team = 'X'
-        }
-    }
-
+    let team
+    
     // CPU Exports
-    return {}
+    return {team}
 
 })()
+
+const coinflipBtn = document.querySelector('#coinflip-button')
+coinflipBtn.addEventListener('click', Controller.assignTeam)
